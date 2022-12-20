9d06e69e00f69315f363327bd8eec5dd70c61cc8
Petr_Ganin <stranik1985@gmail.com>
2022-12-20 15:02:28 +0300

    вводит трехзначное число и удаляет среднюю цифру

---
+++ task1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, котороя выводит случайное трехзначное число и удаляет среднюю цифру.
+int namber = new Random().Next(100,1000);
+Console.WriteLine("трехзначное число: " + namber);
+int namber1 = namber/100;
+int namber2 = namber%10;
+Console.Write(namber1);
+Console.Write(namber2);
