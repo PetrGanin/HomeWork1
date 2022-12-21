bae9c6e877a72c69fd08f24edaca080b4cf0b390
Petr_Ganin <stranik1985@gmail.com>
2022-12-21 20:05:20 +0300

    выдает третью цифру заданного числа

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,19 +1,17 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
-Console.WriteLine("введите трехзначное чиcло");
-string namber = Console.ReadLine();
-string name = "-tom";
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
-if (namber[0] == name[0])
-{
-    Console.WriteLine("вторым числом является");
-    Console.WriteLine(namber[2]);
-}
-else
-{
-    Console.WriteLine("вторым числом является");
-    Console.WriteLine(namber[1]);
-}
+
+int A = new Random().Next(1,1000);
+Console.WriteLine (A);
+int B = A%10;
+if (A < 100)
+  {
+    Console.WriteLine("третьей цыфры нет");
+    return;
+  }
+
+  Console.WriteLine (B);
