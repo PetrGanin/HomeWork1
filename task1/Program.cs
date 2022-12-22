b553a54e73b25271c0472ac5902eb7a556487467
Petr_Ganin <stranik1985@gmail.com>
2022-12-22 11:10:14 +0300

    дни недели с выходными

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,17 +1,36 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
+// 6 -> да
+// 7 -> да
+// 1 -> нет
 
-
-int A = new Random().Next(1,1000);
-Console.WriteLine (A);
-int B = A%10;
-if (A < 100)
-  {
-    Console.WriteLine("третьей цыфры нет");
-    return;
-  }
-
-  Console.WriteLine (B);
+Console.WriteLine("Введите число от 1 до 7: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+switch ((namber))
+{
+    case 1:
+        Console.WriteLine("Понедельник - рабочий");
+        break;
+    case 2:
+        Console.WriteLine("Вторник - рабочий");
+        break;
+    case 3:
+        Console.WriteLine("Среда - рабочий");
+        break;
+    case 4:
+        Console.WriteLine("Четверг - рабочий");
+        break;
+    case 5:
+        Console.WriteLine("Пятница - рабочий");
+        break;
+    case 6:
+        Console.WriteLine("Суббота - выходной");
+        break;
+    case 7:
+        Console.WriteLine("Воскресение - выходной");
+        break;
+}
+if (namber > 7)
+{
+    Console.WriteLine("такого дня недели нет");
+}
